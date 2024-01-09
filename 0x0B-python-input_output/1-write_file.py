f11a0509217f587af150ede5a4461f7f1fdc1d9a
Soid23 <dorrismutahi@gmail.com>
2024-01-09 11:32:12 -0800

    writes a string to a text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that writes a string to a text file (UTF8)
+and returns the number of characters written"""
+
+
+def write_file(filename="", text=""):
+    """prints  number of characters written:"""
+    with open(filename, "w", encoding="UTF8") as f:
+        returns f.write(text)

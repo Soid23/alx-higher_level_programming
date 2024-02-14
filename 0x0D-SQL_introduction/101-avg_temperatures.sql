0341985c3d83f8e0e103d40bf1123e56f309b56e
Soid23 <dorrismutahi@gmail.com>
2024-02-14 00:20:30 -0800

    changes

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,2 @@
--- displays the average temperature in a descending order.
-SELECT `city`, AVG(`value`) AS `avg_temp`
-FROM `temparatures`
-GROUP BY `city`
-ORDER BY `avg_temp` DESC;
+-- Display city temperature in a descending order.
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
